c1c54f15c2bfcdc06acc8f0e20e15bcacdacfd8b
Ian Donlon <iand51@email.arizona.edu>
2020-03-21 22:01:43 -0400

    Styled Navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -4,13 +4,19 @@ import {Navbar,
         NavDropdown,
         Form,
         FormControl,
-        Button} from 'react-bootstrap';
+        Button,
+        Image} from 'react-bootstrap';
 
 
 const MainNavbar = ({navItems}) => {
   return(
     <div>
-    <Navbar expand="lg" className="nav">
+    <Navbar expand="lg" className="nav" sticky="top">
+      <Image
+        src = "../../../static/images/acrp LOGO 031920 2.png"
+        width={210}
+        height={110}
+      />
 
       <Navbar.Toggle className = "ml-auto">
         <div className = "toggleRow"></div>
@@ -19,13 +25,19 @@ const MainNavbar = ({navItems}) => {
       </Navbar.Toggle>
 
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto nav-items">
+        <Nav className="mr-auto">
           {navItems.map((item) => (
             <Nav.Link href={item.ref} replace>
               <span className = "item">{item.page}</span>
             </Nav.Link>
           ))}
         </Nav>
+        <Navbar.Brand className = "ml-auto">
+           <Button className="join-btn">
+             <span>Join</span>
+           </Button>
+
+       </Navbar.Brand>
       </Navbar.Collapse>
     </Navbar>
   </div>
