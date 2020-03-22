c68484a7c066555a445b53b625bcdb5c18490afe
Ian Donlon <iand51@email.arizona.edu>
2020-03-21 23:33:45 -0400

    Footer Styled

---
+++ src/components/buttons/socialButtons.jsx
@@ -0,0 +1,16 @@
+import React from 'React';
+
+
+const FacebookLogo = () => (
+  <div className="social-btn">
+    <a href="#" className="fa fa-facebook"></a>
+  </div>
+);
+
+const InstagramLogo = () => (
+  <div className="social-btn">
+    <a href="#" className="fa fa-instagram"></a>
+  </div>
+);
+
+export {FacebookLogo, InstagramLogo};
--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,11 +1,52 @@
 import React from "react";
+import {Container,
+        Row,
+        Col,
+        Image} from "react-bootstrap";
+import {FacebookLogo, InstagramLogo} from "../buttons/socialButtons";
+
+import siteMap from "../../data/siteMap";
 
 const Footer = ({navItems}) => (
 
     <div>
-      footer
+      <Container className="footer-container" fluid>
+        <Row>
+          <Col xs={12} md={8}>
+            <Image
+              src = "../../../static/images/acrp LOGO 031920 2.png"
+              height={175}
+              width={300}
+              className = "m-auto d-block"
+            />
+          </Col>
+          <Col xs={6} md={4} className = "d-none d-md-block">
+            <Image
+              src = "../../../static/images/perry-grone-lbLgFFlADrY-unsplash.jpg"
+              fluid
+            />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col xs={12} md = {4}>
+            <ul className="footer-map">
+              {siteMap.map((item) => (
+                <li className="footer-map-item">{item.page}</li>
+              ))}
+            </ul>
+          </Col>
+          <Col>
+            <div className="socials-container">
+              <FacebookLogo/>
+              <InstagramLogo/>
+            </div>
+          </Col>
+        </Row>
+        <p align="center">&copy;2020 All Rights Reserved. Site Design by Octavia Social Media Strategies</p>
+      </Container>
     </div>
-  
+
 );
 
 export default Footer;
