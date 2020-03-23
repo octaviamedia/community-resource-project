9de8db20655d72e8757dcc58fc60ec4a8835501f
Ian Donlon <iand51@email.arizona.edu>
2020-03-22 21:13:58 -0400

    Added Video and links

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,9 +14,39 @@ const Index = () => {
             <Button className="site-nav-btn mx-auto d-block"><span>About Us</span></Button>
           </Col>
         </Row>
+
+        <Row>
+          <Col xs={12} md={6}>
+            <h2>Current Update on COVID-19</h2>
+            <p>{content.news}</p>
+            <ol>
+              {content.links.map((link) => (
+                <a href={link}>
+                  <li className="link">{link}</li>
+                </a>
+              ))}
+            </ol>
+          </Col>
+          <Col xs={12} md={6}>
+            <h2>The Importance of Social Distancing</h2>
+            <Iframe
+              link = "https://www.youtube.com/watch?v=-nZMeNP84gM?autoplay=1"
+              credit = "Centers for Disease Control and Prevention (CDC)"
+            />
+          </Col>
+        </Row>
   		</Container>
     </MainLayout>
 	);
 };
+
+const Iframe = ({link, credit}) => (
+  <div className = "iframe">
+    <iframe width="100%" height="300"
+      src={link}>
+    </iframe>
+    <text className = "text-muted">{credit}</text>
+  </div>
+);
 
 export default Index;
